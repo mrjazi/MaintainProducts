23ac774b15bb4e6a3302a2079c34d7acbab44699
Jazi Al-Maghariz <Jazi.Al-Maghariz@icsfs.internal>
2025-03-02 15:13:14 +0300

    crud add fixed (var was the problem)

--- src/main/java/org/primefaces/test/Crud.java
+++ src/main/java/org/primefaces/test/Crud.java
@@ -4,55 +4,66 @@ import jakarta.annotation.PostConstruct;
 import jakarta.faces.application.FacesMessage;
 import jakarta.faces.context.FacesContext;
 import jakarta.faces.view.ViewScoped;
-import jakarta.inject.Inject;
 import jakarta.inject.Named;
 import lombok.Data;
-import org.primefaces.PrimeFaces;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
+import java.util.Arrays;
 
+@Getter
 @Named
 @ViewScoped
 @Data
+
 public class Crud implements Serializable {
 
     private List<Product> products;
+    @Setter
     private Product selectedProduct;
-    private List<Product> selectedProducts;
-
-    @Inject
-    private ProductService productService;
+    private List<String> countries;
+    private List<String> categories;
 
     @PostConstruct
     public void init() {
-        this.products = this.productService.getProducts();
-        this.selectedProducts = new ArrayList<>();
-        this.selectedProduct = new Product();
+        products = new ArrayList<>();
+        selectedProduct = new Product();
+        countries = Arrays.asList("USA", "Canada", "Germany", "France", "Jordan");
+        categories = Arrays.asList("Electronics", "Clothing", "Books");
+        products.add(new Product(
+                "123456789",
+                "Book",
+                new BigDecimal("19.99"),
+                "uuu",
+                "New",
+                2,
+                "Jo",
+                "Books",
+                "A great book."
+        ));
     }
 
     public void addProduct() {
         if (selectedProduct != null) {
-            selectedProduct.setProductId(products.size() + 1);
 
-            this.products.add(selectedProduct);
+            if (products.stream().anyMatch(i -> i.getProductId().equals(selectedProduct.getProductId()))) {
+                FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error", "Product with the same ID already exists."));
+                return;
+            }
 
-            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Product Added"));
+            products.add(selectedProduct.clone());
 
-            this.selectedProduct = new Product();
+            System.out.println("---------------------->>>>>>>>>>>>" + selectedProduct);
+            System.out.println("-------------product--------->>>>>>>>>>>>" + products);
 
-            PrimeFaces.current().ajax().update("form:msgs", "form:data-table-products");
-        }
-    }
+            selectedProduct = new Product();
+            selectedProduct.init();
 
-    public void deleteProduct() {
-        if (selectedProduct != null) {
-            this.products.remove(selectedProduct);
-            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Product Removed"));
-            PrimeFaces.current().ajax().update("form:data-table-products");
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Success", "Product added successfully."));
         }
     }
 }
--- src/main/java/org/primefaces/test/Product.java
+++ src/main/java/org/primefaces/test/Product.java
@@ -11,62 +11,35 @@ import lombok.Getter;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
 
 @Named
 @ViewScoped
 @Data
 public class Product implements Serializable {
 
-    private int productId;
+    private String productId;
     private String productName;
     private BigDecimal productAmount;
-    private Date productDate;
-    private List<String> productType;
+    private String productDate;
+
     private String type;
     private int quantity;
-    private List<String> productCountry;
+
     private String country;
-    private List<String> productCategory;
+
     private String category;
     private String productDescription;
 
 
     public Product() {
-
     }
 
     @PostConstruct
     public void init() {
-//        productId = 0;
-//        quantity = 0;
-        productAmount = new BigDecimal(0);
-
-
-        productCountry = new ArrayList<String>();
-        productCountry.add("Jordan");
-        productCountry.add("United States");
-        productCountry.add("Canada");
-        productCountry.add("France");
-        productCountry.add("Germany");
-        productCountry.add("Italy");
-
-
-        productCategory = new ArrayList<String>();
-        productCategory.add("Clothes");
-        productCategory.add("Food");
-
-
-        productType = new ArrayList<String>();
-        productType.add("New");
-        productType.add("Old");
-
 
     }
 
-    public Product(int productId, String productName, BigDecimal productAmount, Date productDate, String type, int quantity, String country, String category, String productDescription) {
+    public Product(String productId, String productName, BigDecimal productAmount, String productDate, String type, int quantity, String country, String category, String productDescription) {
         this.productId = productId;
         this.productName = productName;
         this.productAmount = productAmount;
@@ -78,10 +51,10 @@ public class Product implements Serializable {
         this.productDescription = productDescription;
     }
 
-//    @Override
-//    public Product clone() {
-//        return new Product(getProductId(), getProductName(), getProductAmount(), getProductDate(), getProductType(), getQuantity(), getProductCountry(), getProductCategory(), getProductDescription());
-//    }
+    @Override
+    public Product clone() {
+        return new Product(getProductId(), getProductName(), getProductAmount(), getProductDate(), getType(), getQuantity(), getCountry(), getCategory(), getProductDescription());
+    }
 
 
 //    public void onCountryChange() {
@@ -90,16 +63,33 @@ public class Product implements Serializable {
 //        }
 //    }
 
-    public void displayLocation() {
-        FacesMessage msg;
-        if (category != null && country != null) {
-            msg = new FacesMessage("Selected", category + " of " + country);
-        }
-        else {
-            msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Invalid", "City is not selected.");
-        }
 
-        FacesContext.getCurrentInstance().addMessage(null, msg);
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((productId == null) ? 0 : productId.hashCode());
+        return result;
     }
 
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        Product other = (Product) obj;
+        if (productId == null) {
+            return other.productId == null;
+        }
+        else {
+            return productId.equals(other.productId);
+        }
+    }
 }
