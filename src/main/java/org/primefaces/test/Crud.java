e2e4824ae8e4c76f4f820e25e8eeb72f545e16e7
Jazi Al-Maghariz <Jazi.Al-Maghariz@icsfs.internal>
2025-03-03 14:13:57 +0300

    sort add view update added

--- src/main/java/org/primefaces/test/Crud.java
+++ src/main/java/org/primefaces/test/Crud.java
@@ -8,6 +8,7 @@ import jakarta.inject.Named;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+import org.primefaces.PrimeFaces;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
@@ -29,12 +30,16 @@ public class Crud implements Serializable {
 
     private List<String> countries;
     private List<String> categories;
+    private List<Product> selectedProducts;
+
+    private List<Product> productToView;
 
     @PostConstruct
     public void init() {
         products = new ArrayList<>();
         selectedProduct = new Product();
-
+        this.selectedProducts = new ArrayList<Product>();
+        productToView = new ArrayList<>();
 
         countries = Arrays.asList("USA", "Canada", "Germany", "Jordan");
         categories = Arrays.asList("Electronics", "Clothing", "Books");
@@ -65,7 +70,7 @@ public class Crud implements Serializable {
             selectedProduct.setProductDate(LocalDate.parse(today.format(formatPattern), formatPattern));
 
 
-            System.out.println("---------------->>>>>>>> "+selectedProduct.getProductDate());
+            System.out.println("---------------->>>>>>>> " + selectedProduct.getProductDate());
 
             products.add(selectedProduct.clone());
 
@@ -78,4 +83,55 @@ public class Crud implements Serializable {
             FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Success", "Product added successfully."));
         }
     }
+
+
+    public void saveProduct() {
+        if (this.selectedProduct.getProductId() != null) {
+
+            DateTimeFormatter formatPattern = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+            LocalDate today = selectedProduct.getProductDate();
+            String formatDate = today.format(formatPattern);
+
+            this.products.add(this.selectedProduct);
+            this.products.remove(this.selectedProduct);
+            this.selectedProducts.remove(this.selectedProduct);
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Success", "Product saved successfully."));
+
+            PrimeFaces.current().executeScript("PF('manageProductDialog').hide()");
+            PrimeFaces.current().ajax().update("form:msgs", "form");
+        }
+    }
+
+    public void deleteProduct() {
+        this.products.remove(this.selectedProduct);
+        this.selectedProducts.remove(this.selectedProduct);
+//        this.selectedProduct = null;
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Product " + this.selectedProduct.getProductName() + " removed"));
+        PrimeFaces.current().ajax().update("form:msgs", "form:data-table-products");
+    }
+
+//    public void deleteSelectedProducts() {
+//        if (this.selectedProducts != null && !this.selectedProducts.isEmpty()) {
+//            this.products.removeAll(this.selectedProducts);
+//            this.selectedProducts.clear();
+//            FacesContext.getCurrentInstance().addMessage(null,
+//                    new FacesMessage("Products Removed"));
+//            PrimeFaces.current().ajax().update("form:msgs", "form:data-table-products");
+//        }
+//    }
+
+
+    public String viewProduct(String productId) {
+        this.productToView = new ArrayList<>();
+
+        System.out.println("------------------------->>> " + selectedProduct);
+
+        this.productToView.add(selectedProduct);
+
+        System.out.println("-----------product----------->>>>>>>>>>" + productToView);
+
+        return productToView.toString();
+    }
+
+
 }
